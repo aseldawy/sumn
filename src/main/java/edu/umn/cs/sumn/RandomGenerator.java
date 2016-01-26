d6a12cf1818c70de3ddd2517a9ea47243ab485ad
Ahmed Eldawy <eldawy@cs.umn.edu>
2016-01-26 11:14:49 -0600

    Updated the random number generator to take the parameter (delta)

--- src/main/java/edu/umn/cs/sumn/RandomGenerator.java
+++ src/main/java/edu/umn/cs/sumn/RandomGenerator.java
@@ -10,10 +10,12 @@ public class RandomGenerator {
     private static final long MANTISSA_MASK = 0x000FFFFFFFFFFFFFL;
     private static final int EXP_MASK = 0x7FF;
     private static final int MANTISSA_BITS = 52;
+    
     public static void main(String[] args) throws FileNotFoundException {
         Random random = new Random();
 
         long numRecords = 10000000;
+        int delta = 10;
         
         PrintWriter out = new PrintWriter(new FileOutputStream(new File("big_file.txt")));
         
@@ -21,7 +23,7 @@ public class RandomGenerator {
           boolean negative = false; // Always positive
           long mantissa = Math.abs(random.nextLong()) & MANTISSA_MASK;
           // We don't generate the value 7FF as it represents Infinity and NaN
-          int exp = random.nextInt(EXP_MASK);
+          int exp = random.nextInt(delta);
           
           long value = negative ? 0x8000000000000000L : 0;
           value |= mantissa;
